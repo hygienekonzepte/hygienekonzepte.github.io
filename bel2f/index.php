26333dbc1bcbbed97ec8102ca7e4c03a2b4b144c
Holger Wahlen <Tarquin@web.de>
2020-09-03 23:46:40 +0200

    Initial.

---
+++ bel2f/index.php
@@ -0,0 +1,21 @@
+<?php
+$title = 'Hygienekonzepte BeL 2 Frauen';
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title><?= htmlspecialchars($title) ?></title>
+<link rel="stylesheet" href="../style.css"/>
+</head>
+<body>
+<p class="logo"><img src="../wvv-logo.jpg"/></p>
+<h1><?= htmlspecialchars($title) ?></h1>
+
+<ul>
+
+<li><p><a href="mtv.pdf">MTV</a></p></li>
+
+</ul>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$title = 'Hygienekonzepte';
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title><?= htmlspecialchars($title) ?></title>
+<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<p class="logo"><img src="wvv-logo.jpg"/></p>
+<h1><?= htmlspecialchars($title) ?></h1>
+
+<ul>
+
+<li><p><a href="bel2f/">BeL 2 Frauen</a></p></li>
+
+</ul>
+
+</body>
+</html>
